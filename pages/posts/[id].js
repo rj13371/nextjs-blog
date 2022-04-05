c5559ea689dbeb7ebad1b7ef5514765115c83f7a
rj1337 <mathesonroland@gmail.com>
2022-04-05 18:36:11 +0900

    add api endpoint

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,13 +1,35 @@
 import Layout from '../../components/layout'
 
-export default function Post() {
-    return <Layout>...</Layout>
-}
-
-export async function getStaticPaths() {
-    // Return a list of possible value for id
-}
+import { getAllPostIds, getPostData } from '../../lib/posts'
 
 export async function getStaticProps({ params }) {
-    // Fetch necessary data for the blog post using params.id
+    const postData = await getPostData(params.id)
+    return {
+        props: {
+            postData
+        }
+    }
+}
+
+
+export async function getStaticPaths() {
+    const paths = getAllPostIds()
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export default function Post({ postData }) {
+    return (
+        <Layout>
+            {postData.title}
+            <br />
+            {postData.id}
+            <br />
+            {postData.date}
+            <br />
+            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        </Layout>
+    )
 }
